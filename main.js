d2bedf411fe29daee4463234c839903f95c9066d
Hyclo <miguel-aroldi@bluewin.ch>
2022-10-04 11:39:10 +0200

    added functions to add and delete tasks

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function displayTasks(tasks) {
     taskDiv.classList.add("block");
 
     const titleDiv = document.createElement("div");
-    
+
     const buttonDiv = document.createElement("div");
     buttonDiv.classList.add("container");
 
@@ -35,11 +35,15 @@ function displayTasks(tasks) {
     radioButton.type = "radio";
 
     const deleteButton = document.createElement("button");
-    deleteButton.innerText = "Del"
+    deleteButton.innerText = "Del";
     deleteButton.classList.add("button");
+    deleteButton.onclick = () => {
+		deleteTask(task.id);
+        displayTasks(tasks)
+	}
 
     const alterButton = document.createElement("button");
-    alterButton.innerText = "Alter"
+    alterButton.innerText = "Alter";
     alterButton.classList.add("button");
 
     const delDiv = document.createElement("div");
@@ -50,42 +54,80 @@ function displayTasks(tasks) {
     radioDiv.classList.add("inline");
     delDiv.append(deleteButton);
     alterDiv.append(alterButton);
-    radioDiv.append(radioButton)
+    radioDiv.append(radioButton);
 
-    buttonDiv.append(radioDiv, delDiv, alterDiv)
+    buttonDiv.append(radioDiv, delDiv, alterDiv);
     taskDiv.append(titleDiv, buttonDiv);
 
-
     if (task.completed == false) {
-        openTasks.append(taskDiv)
+      openTasks.append(taskDiv);
     } else {
-        closedTasks.append(taskDiv)
+      closedTasks.append(taskDiv);
     }
   }
 }
 
 function validate(document) {
-    const mailRegex = "\\S{1,}@\\S{1,}\\.\\S{2,4}";
-    if (document.id == "eMail") {
-      if (document.value.match(mailRegex)) {
-        return true;
-      }
-      alert("please enter valid E-Mail");
-      return false;
+  const mailRegex = "\\S{1,}@\\S{1,}\\.\\S{2,4}";
+  if (document.id == "eMail") {
+    if (document.value.match(mailRegex)) {
+      return true;
     }
-  
-    if (
-      document.value == null ||
-      document.value == undefined ||
-      document.value == NaN ||
-      document.value == ""
-    ) {
-      alert("Please enter a valid value!");
-      return false;
-    }
-    return true;
+    alert("please enter valid E-Mail");
+    return false;
   }
 
-function addTask(){
-    window.location.href = "addTask.html"
+  if (
+    document.value == null ||
+    document.value == undefined ||
+    document.value == NaN ||
+    document.value == ""
+  ) {
+    alert("Please enter a valid value!");
+    return false;
+  }
+  return true;
+}
+
+function loadDataFromBackend() {
+	fetch("http://localhost:3000/contacts").then(function (data) {
+		return data.json()
+	}).then(function (json) {
+		tasks = json
+		displayTasks(tasks)
+	})
+}
+
+function addTask() {
+  const todoForm = document.getElementById("addTodo");
+  const todoTitle = document.getElementById("TodoTitleTask");
+  todoTitle.innerText = "ADD TODO";
+
+  validate(todoForm);
+
+  const body = {
+    id: 10,
+    completed: false,
+    title: todoForm.value,
+  };
+
+  fetch("http://localhost:3000/tasks", {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body)
+  }).then(function () {
+    window.location.href = "index.html";
+    loadDataFromBackend()
+});
+}
+
+function deleteTask(id){
+    fetch(`http://localhost:3000/task/${id}`, {
+        method: "DELETE",
+      }).then(function () {
+        window.location.href = "index.html";
+        loadDataFromBackend()
+    });
 }
