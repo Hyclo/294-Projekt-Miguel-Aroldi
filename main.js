52fe5f6b64a8f63acd79dc6f40984543538f69d1
Hyclo <miguel-aroldi@bluewin.ch>
2022-10-04 10:43:12 +0200

    added js to display tasks

---
+++ main.js
@@ -0,0 +1,70 @@
+let tasks = [];
+
+fetch("http://localhost:3000/tasks")
+  .then(function (data) {
+    return data.json();
+  })
+  .then(function (json) {
+    tasks = json;
+    displayTasks(tasks);
+  });
+
+function displayTasks(tasks) {
+  const openTasks = document.getElementById("openTasks");
+  const closedTasks = document.getElementById("closedTasks");
+  openTasks.replaceChildren();
+  closedTasks.replaceChildren();
+
+  for (let taskIndex in tasks) {
+    const task = tasks[taskIndex];
+
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("block");
+
+    const titleDiv = document.createElement("div");
+    
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("container");
+
+    const title = document.createElement("h3");
+    title.innerText = `${task.id} ${task.title}`;
+    title.classList.add("normaltext");
+    titleDiv.append(title);
+
+    const radioButton = document.createElement("input");
+    radioButton.type = "radio";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Del"
+    deleteButton.classList.add("button");
+
+    const alterButton = document.createElement("button");
+    alterButton.innerText = "Alter"
+    alterButton.classList.add("button");
+
+    const delDiv = document.createElement("div");
+    delDiv.classList.add("inline");
+    const alterDiv = document.createElement("div");
+    alterDiv.classList.add("inline");
+    const radioDiv = document.createElement("div");
+    radioDiv.classList.add("inline");
+    delDiv.append(deleteButton);
+    alterDiv.append(alterButton);
+    radioDiv.append(radioButton)
+
+    buttonDiv.append(radioDiv, delDiv, alterDiv)
+    taskDiv.append(titleDiv, buttonDiv);
+
+
+    if (task.completed == false) {
+        openTasks.append(taskDiv)
+    } else {
+        closedTasks.append(taskDiv)
+    }
+  }
+}
+
+function addTask(){
+    window.location.href = 
+
+}
